5ed1ea1cee1c644b55f511e6d0ea0f9dbca14063
arvind79 <81362519+arvind79@users.noreply.github.com>
2023-04-25 23:21:38 +0530

    started working on app layout. Added header

--- App.js
+++ App.js
@@ -1,46 +1,49 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import logo from "./assets/img/foodvilla.png"
 
-//Element
-const title = (
-    <h2 id="title" key="h2">
-        Namaste React
-    </h2>
-);
-
-//Functional Component - this is returning the JSX and this component has arrow function
 const Title = () => (
-    <h2 id="title" key="h2">
-        Namaste React
-    </h2>
+    <img className="logo" src={logo} alt="logo"></img>
 );
 
-// React Component
-// - Functional - NEW - I will use this most of the time
-// - Class Based Component - OLD
-
-// Functional Components
-// Name of component starts with capital letters - it's not mandatory, but it is good practice
-// Functional component is a normal javascript function
-// It is just returning a piece of jsx or react createElement
-
-// <Title /> is known as Composing Components or Component Composition
-const HeaderComponent = () => {
+const Header = () => {
     return (
         <div>
-            {title}
             <Title />
-            {/*OR  {Title()}  Inside these outer curly braces it is javascript*/}
-            {/* {We can write any piece of javascript code inside these curly braces} */}
-            {console.log(1+2+5, "any js code")}
-            <h1>Namaste React functional component</h1>
-            <h2>This is a h2 tag</h2>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
         </div>
     );
 }; 
 
+const Body = () => {
+    return (
+        <h4>Body</h4>
+    )
+}
+
+const Footer = () => {
+    return (
+        <h4>Footer</h4>
+    )
+}
+
+const AppLayout = () => {
+    return (
+        <>
+            <Header />
+            <Body />
+            <Footer />
+        </>
+    )
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// root.render(heading); //heading is react element
-root.render(<HeaderComponent />) //HeaderComponent is a functional component
-// OR root.render(HeaderComponent())
+root.render(<AppLayout />)
