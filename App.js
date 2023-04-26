97ef93b1860f72506323870c388ca5dc9d24b95c
arvind79 <81362519+arvind79@users.noreply.github.com>
2023-04-26 09:46:02 +0530

    hard coded data removed and array of objects with restaurantList data added

--- App.js
+++ App.js
@@ -24,21 +24,77 @@ const Header = () => {
     );
 }; 
 
-const RestrauntCard = () => {
+//Config Driven UI
+const config = [
+    {
+        type: "carousel",
+        cards: [
+            {
+                offerName: "50% off"
+            },
+            {
+                offerName: "No Delivery Charge"
+            }
+        ]
+    },
+    {
+        type: "restaurants",
+        cards: [
+            {
+                offerName: "50% off"
+            },
+            {
+                offerName: "No Delivery Charge"
+            }
+        ]
+    }
+
+]
+
+const restaurantList = [
+    {
+        name: "Burger King",
+        image: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/28fb13049b4e55297bb3f703cde63c35",
+        cuisines: ["Burger", "American"],
+        rating: "4.2",
+    },
+    {
+        name: "Burger King",
+        image: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/28fb13049b4e55297bb3f703cde63c35",
+        cuisines: ["Burger", "American"],
+        rating: "4.2",
+    },
+    {
+        name: "Burger King",
+        image: "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/28fb13049b4e55297bb3f703cde63c35",
+        cuisines: ["Burger", "American"],
+        rating: "4.2",
+    }, 
+]
+
+const RestaurantCard = () => {
     return (
         <div className="card">
-            <img src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/28fb13049b4e55297bb3f703cde63c35"/>
-            <h2>Burger King</h2>
-            <h3>Burgers, American</h3>
-            <h4>4.2 stars</h4>
+            <img src={restaurantList[0].image} />
+            <h2>{restaurantList[0].name}</h2>
+            <h3>{restaurantList[0].cuisines.join(", ")}</h3>
+            <h4>{restaurantList[0].rating} stars</h4>
         </div>
     )
 }
 
 const Body = () => {
     return (
-        <div>
-            <RestrauntCard />
+        <div className="restaurant-list">
+            <RestaurantCard />
+            <RestaurantCard />
+            <RestaurantCard />
+            <RestaurantCard />
+            <RestaurantCard />
+            <RestaurantCard />
+            <RestaurantCard />
+            <RestaurantCard />
+            <RestaurantCard />
         </div>
     )
 }
