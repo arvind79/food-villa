33f260181366138c11464e0ba60015c57def3177
arvind79 <81362519+arvind79@users.noreply.github.com>
2023-05-11 22:52:54 +0530

    using createBrowserRouter for the routing of our website pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import Header from "./components/Header" //Default import
-// import { Header } from "./components/Header"  //named import
-// import * as XYZ from "./components/Header"  //importing everything from file and using as XYZ.Header , XYZ.Title and so on...
-
+import Header from "./components/Header"
 import Body from "./components/Body"
 import Footer from "./components/Footer"
+import About from "./components/About"
+import {createBrowserRouter, RouterProvider, } from "react-router-dom"
 
 const AppLayout = () => {
   return (
@@ -17,6 +16,17 @@ const AppLayout = () => {
   )
 }
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+])
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(<AppLayout />)
+root.render(<RouterProvider router={appRouter} />)
