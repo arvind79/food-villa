27fa4ee98c4358cd6c341e0b479e7621e2ffdfde
arvind79 <81362519+arvind79@users.noreply.github.com>
2023-05-13 17:48:38 +0530

    added Profile component as a children of About Component
    With the help of react-router-dom.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import About from "./components/About"
 import Error from "./components/Error"
 import Contact from "./components/Contact"
 import RestaurantMenu from "./components/RestaurantMenu"
+import Profile from "./components/Profile"
 import {createBrowserRouter, RouterProvider, Outlet} from "react-router-dom"
 
 const AppLayout = () => {
@@ -32,6 +33,12 @@ const appRouter = createBrowserRouter([
       {
         path: "/about",
         element: <About />,
+        children: [
+          {
+            path: "profile",
+            element: <Profile />,
+          },
+        ]
       },
       {
         path: "/contact",
--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,16 @@
+import { Outlet } from "react-router-dom"
+import ProfileFunctionalComponent from "./Profile"
+import Profile from "./ProfileClass"
+
 const About = () => {
   return (
     <div>
       <h1>About Us Page</h1>
       <p>
-        {" "}
         This is about the page which user can see to know about this website
       </p>
+      <ProfileFunctionalComponent />
+      <Profile />
     </div>
   )
 }
