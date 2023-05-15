46768f276fa12009bc8223f614124d547607a8b3
arvind79 <81362519+arvind79@users.noreply.github.com>
2023-05-15 11:55:09 +0530

    using constructor and state in class component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,13 +1,22 @@
 import { useState } from "react";
 
 const Profile = (props) => {
-  const [count] = useState(0)
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    // API call
+    //console.log("useEffect()")
+  })
+  //console.log("render")
 
   return (
     <div>
       <h2>Profile Functional Component</h2 >
       <h3>Name: {props.name}</h3>
       <h3>Count: {count}</h3>
+      <button onClick={() => {
+        setCount(1)
+      }}>Click</button>
     </div>
   )
 };
--- src/components/ProfileClass.js
+++ src/components/ProfileClass.js
@@ -1,11 +1,32 @@
 import React from "react";
 
 class Profile extends React.Component {
+  constructor(props) {
+    super(props)
+    //Create state
+    this.state = {
+      count: 0,
+      count2: 0,
+    }
+    console.log("Constructor")
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount")
+  }
+
   render() {
+    console.log("render")
     return (
       <div>
         <h1>Profile Class Component</h1>
         <h2>Name: {this.props.name}</h2>
+        <h2>Count: {this.state.count}</h2>
+        <button onClick={() => {
+          this.setState({
+            count: 1,
+          })
+        }}>setCount</button>
       </div>
     );
   }
